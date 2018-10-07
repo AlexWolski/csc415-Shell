ccedf95ad4e5e5f4da60a06ed287b321e2fc53a4
Alex Wolski <alex.theinfinite@gmail.com>
2018-10-06 18:40:31 -0700

    Shell 2 Done

--- myshell.c
+++ myshell.c
@@ -1,7 +1,7 @@
 /****************************************************************
- * Name        :                                                *
+ * Name        : Alex Wolski                                    *
  * Class       : CSC 415                                        *
- * Date        :                                                *
+ * Date        : 10/20/18                                       *
  * Description :  Writting a simple bash shell program          *
  *                that will execute simple commands. The main   *
  *                goal of the assignment is working with        *
@@ -17,12 +17,42 @@
 #include <fcntl.h>
 
 #define BUFFERSIZE 256
+#define MAXPARAMS 5
 #define PROMPT "myShell >> "
 #define PROMPTSIZE sizeof(PROMPT)
 
 int main(int* argc, char** argv)
 {
+  int numArgs;
+  char** arguments = malloc(BUFFERSIZE);
+  char* input = malloc(BUFFERSIZE);
+  cosnt char* delims = " \n";
 
+  while (1)
+  {
+    numArgs = 0;
+    memset(arguments, 0, BUFFERSIZE);
     
-return 0;
+    printf("%s", PROMPT);
+    fgets(input, BUFFERSIZE, stdin);
+
+    if(feof(stdin))
+    {
+      printf("\n");
+      exit(0);
+    }
+    
+    if(strcmp(input, "exit\n") == 0)
+      exit(0);
+
+    arguments[0] = strtok(input, delims);
+    
+    while(arguments[numArgs] != NULL)
+    {
+      numArgs += 1;
+      arguments[numArgs] = strtok(NULL, delims);
+    }
+  }
+  
+  return 0;
 }
