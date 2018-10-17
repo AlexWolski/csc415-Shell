fbd5bca220ed140bcefc3d7550bdc1f8d5f02a57
Alex Wolski <alex.theinfinite@gmail.com>
2018-10-16 22:23:00 -0700

    shell 1 done

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # CSC 415 - Project 3 - My Shell
 
-## Student Name: Name here
-
-## Student ID: ID Here
+Student Name: Alex Wolski
+Student ID: 918276364
 
 ## Build Instructions
+1. Open the terminal and change the directory to inside "csc415-p3-AlexWolski"
+2. Run the command "make" or "make myshell"
 
 ## Run Instructions
+Run the command "./myshell"
+Enter a linux command you want to run	(eg. ls -al)
+You can also pipe commands together	(eg. ls -al | more)
 
 ## List Extra Credits comepleted (if non attempted leave blank)
--- myshell.c
+++ myshell.c
@@ -24,34 +24,47 @@
 int main(int* argc, char** argv)
 {
   int numArgs;
+  int pid;
   char** arguments = malloc(BUFFERSIZE);
   char* input = malloc(BUFFERSIZE);
-  cosnt char* delims = " \n";
+  const char* delims = " \n";
 
   while (1)
   {
+    //Reset the variables modified in the last loop. "arguments" is populated with null bytes.
     numArgs = 0;
     memset(arguments, 0, BUFFERSIZE);
-    
+
+    //Prompt the user for an input and read it
     printf("%s", PROMPT);
     fgets(input, BUFFERSIZE, stdin);
 
+    //If the user pressed CTRL+D, exit the program
     if(feof(stdin))
     {
       printf("\n");
       exit(0);
     }
-    
+
+    //If the user enters "exit", exit the program
     if(strcmp(input, "exit\n") == 0)
       exit(0);
 
+    //Set the first index of the arguments array to the command
     arguments[0] = strtok(input, delims);
-    
+
+    //Loop and populate the array with the arguments
     while(arguments[numArgs] != NULL)
     {
       numArgs += 1;
       arguments[numArgs] = strtok(NULL, delims);
     }
+
+    //Execute the command
+    if(fork() == 0)
+      execvp(arguments[0], arguments);
+    else
+      wait(NULL);
   }
   
   return 0;
